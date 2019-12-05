22aad7df16bf5e6083f79e3d52e74bcd66890cf8
Rakimba <aharuna38@gmail.com>
2019-12-05 11:09:34 +0200

    second commit

--- COURSEINFO/src/index.js
+++ COURSEINFO/src/index.js
@@ -20,23 +20,6 @@ return(
 )
 
 }
-const Content = (props)=>{
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
-
-    return (
-         <div>
-         <Part part = {part1} exercise={exercises1}/>
-        <Part part = {part2} exercise={exercises2}/>
-        <Part part = {part3} exercise={exercises3}/>
-         </div>
-    )
-
-}
 
 const Total = (props)=>{
 
@@ -50,10 +33,26 @@ return(
 }
 const App = () => {
     const course = 'Half Stack application development'
-        const exercises1 = 10
-        const exercises2 = 7
-        const exercises3 = 14
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
 
+    const Content = ()=>{
+    
+
+      return (
+           <div>
+          <Part part = {part1} exercise={exercises1}/>
+          <Part part = {part2} exercise={exercises2}/>
+          <Part part = {part3} exercise={exercises3}/>
+           </div>
+      )
+  
+  }
+    
     return (
       <div>
         <Header course={course}/>
