70cf3451484b0a6913a801986f6502a2000fe5a2
Rakimba <aharuna38@gmail.com>
2019-12-11 12:56:47 +0200

    1.9 : unicafe step 4

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,39 +1,43 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Average = (props)=>{
-   const ave = props.add/props.total
-   if(props.total ===0){
-       return (
-           <div>average 0</div>
-       )
-   }
-  return(
-      <div>
-<p>average {ave}</p>
-      </div>
-  )
-}
 
-const Positive =(props)=>{
-    const positive = props.good/props.total
+const Statistics =(props)=>{
+  
     if(props.total ===0){
-        return (
-            <div>Positive 0 %</div>
-        )
-    }
-   return(
-       <div>
- <p>Positive {positive} %</p>
-       </div>
-   )
+      return (
+          <div>
+              <h1>statistics</h1>
+              <p>No feedback given</p>
+          </div>
+      )
 
+    }
+  
+
+    return(
+
+<div>
+            <h1>statistic</h1>
+
+            <p>good {props.good}</p>
+            <p>neutral {props.neutral}</p>
+            <p> bad {props.bad}</p>
+            <p>total {props.total}</p>
+             <p> Average {props.add/props.total}</p>
+             <p>Positive {props.good/props.total} % </p>
+            
+        </div>
+
+    )
 }
+
 const App = ()=>{
 const[good, setgood]= useState(0)
 const[neutral, setneutral]= useState(0)
 const[bad, setbad]= useState(0)
 const [total, settotal]= useState(0)
+
 
 const handleGood =()=>{
     setgood(good +1)
@@ -51,6 +55,9 @@ const handleBad =()=>{
 
 }
 
+const add = Math.abs(good+neutral-bad)
+
+
 return(
   <div>
       <h1>give feedback</h1>
@@ -59,18 +66,7 @@ return(
           <button onClick = {handleNeutral}>neutral</button>
           <button onClick = {handleBad}>bad</button>
         </div>
-        <div>
-            <h1>statistic</h1>
-
-            <p>good  {good}</p>
-            <p>neutral {neutral}</p>
-            <p> bad {-bad}</p>
-            <p>total {total}</p>
-            <Positive good ={good} total = {total}/>
-            <Average add={Math.abs(good+neutral-bad)} total = {total}/>
-
-        </div>
-        
+        <Statistics add ={add} good={good} neutral={neutral} bad ={bad} total={total} />
   </div>
 
 
